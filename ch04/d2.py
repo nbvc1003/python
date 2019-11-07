f3c75c4c005b69c437e204b1929e8bb29e5ee08a
thejonPC <nbvc@nate.com>
2019-11-07 15:00:52 +0900

    20191107 강의내용

--- ch04/d2.py
+++ ch04/d2.py
@@ -1,8 +1,12 @@
-
-
+import sys
+import os
 print('정수를 입력하세요')
 
 num = int(input())
 
 if num != 0:
     print('{0:d} / 3 = {1:0.2f}'.format(num, round(num/3),2))
+
+for i in range(len(sys.path)):
+    print(sys.path[i])
+
