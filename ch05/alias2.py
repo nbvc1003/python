a4a3a349209d272ad1f6bbc8c585a5e16d231fea
thejonPC <nbvc@nate.com>
2019-11-12 14:34:36 +0900

    20191112 강의내용

--- ch05/alias2.py
+++ ch05/alias2.py
@@ -2,8 +2,15 @@
 # call by reference
 # 대표적으로 배열, 함수객체
 x = [2,3,5,7,11]
-y = x
+y = x  # 기존 리스트의 주소를 복사한다.
 y[2] = 4
 
 print(x)
 print(y)
+
+# 새로운 list를 만든다.
+z = list(x)
+z[2] = 3
+print(x)
+print(z)
+
