2a2edad589542128526a995452138fb9fdd0a878
thejonPC <nbvc@nate.com>
2019-12-12 16:20:34 +0900

    20191212 그리는 함수 사용법 한글등등

--- ch17/line_syle2.py
+++ ch17/line_syle2.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib import font_manager, rc, rcParams
+
+# 한글 입력 가능하도록
+font_name = font_manager.FontProperties(
+    fname="c:/windows/Fonts/malgun.ttf").get_name()
+rc('font',family=font_name)
+rcParams['axes.unicode_minus'] = False # -표시 보이게
 
 s1 = pd.Series([84900, 81800, 71756, 92000])
 s2 = pd.Series([80500, 82800, 71736, 90000])
@@ -9,6 +16,7 @@ plt.figure(figsize=(6,4))
 plt.plot(s1, label='04-10', lw=2,c='r',ls='-.',marker='s')
 plt.plot(s2, label='04-11',ls=':',color="#F15F5F")
 
+plt.title('타이틀- ')
 plt.xlim(0.0,4.0)
 plt.ylim(70000, 90000)
 
